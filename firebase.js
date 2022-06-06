5ce9570ebe49f569a58f80a10bac7ea13bd7f945
Angelina Baranova <angelinabar52@gmail.com>
2022-06-06 18:34:05 +0300

    save model to database

---
+++ firebase.js
@@ -0,0 +1,169 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
+
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/9.8.1/firebase-auth.js";
+import {
+  getDatabase,
+  ref,
+  set,
+  update,
+} from "https://www.gstatic.com/firebasejs/9.8.1/firebase-database.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyABHPMIaSStJbYY8wKz8ml7C14dv4jiukA",
+  authDomain: "todo-7eee9.firebaseapp.com",
+  projectId: "todo-7eee9",
+  storageBucket: "todo-7eee9.appspot.com",
+  messagingSenderId: "791119799586",
+  appId: "1:791119799586:web:50df20db598890a646717b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth(app);
+const signUpFormBtn = document.querySelector("#signUpFormBtn");
+const authFormBtn = document.querySelector("#authFormBtn");
+const logoutBtn = document.querySelector("#logoutbtn");
+const authPopupInner = document.querySelector(".auth__inner");
+const errorPopup = document.querySelector(".error-message");
+const errorPopupTxt = errorPopup.querySelector(".error-message__txt");
+
+function markInputsAsErrored(inputs) {
+  for (const input of inputs) {
+    input.classList.add("errored");
+  }
+}
+
+function unMarkInputsAsErrored(inputs) {
+  for (const input of inputs) {
+    input.classList.remove("errored");
+  }
+}
+
+signUpFormBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  errorPopup.classList.add("hidden");
+  let email = document.querySelector("#userMail").value;
+  let password = document.querySelector("#userPassword").value;
+  let name = document.querySelector("#userName").value;
+  const authWindow = document.querySelector(".auth");
+  const userNameInHeader = document.querySelector(".user-manu__username");
+  const signUpInputsContainer = document.querySelector(".signup__inputs");
+  const signUpInputs = signUpInputsContainer.querySelectorAll(".input");
+
+  unMarkInputsAsErrored(signUpInputs);
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      const userId = user.uid;
+      // ...
+
+      //write to database
+
+      set(ref(database, "users/" + userId), {
+        email,
+        name,
+      });
+
+      alert("user signed");
+      authWindow.classList.add("hidden");
+      userNameInHeader.textContent = email;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+      authPopupInner.classList.add("swing");
+      errorPopup.classList.remove("hidden");
+      errorPopupTxt.textContent = errorMessage;
+      markInputsAsErrored(signUpInputs);
+      setTimeout(() => {
+        authPopupInner.classList.remove("swing");
+      }, 500);
+    });
+});
+
+authFormBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  errorPopup.classList.add("hidden");
+  let email = document.querySelector("#userMailSigned").value;
+  let password = document.querySelector("#userPasswordSigned").value;
+  const authWindow = document.querySelector(".auth");
+  const userNameInHeader = document.querySelector(".user-manu__username");
+  const authInputsContainer = document.querySelector(".auth__inputs");
+  const authInputs = authInputsContainer.querySelectorAll(".input");
+
+  unMarkInputsAsErrored(authInputs);
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+      alert("user signed in");
+      authWindow.classList.add("hidden");
+      userNameInHeader.textContent = email;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+      authPopupInner.classList.add("swing");
+      errorPopup.classList.remove("hidden");
+      errorPopupTxt.textContent = errorMessage;
+      markInputsAsErrored(authInputs);
+      setTimeout(() => {
+        authPopupInner.classList.remove("swing");
+      }, 500);
+    });
+});
+
+logoutBtn.addEventListener("click", (e) => {
+  errorPopup.classList.add("hidden");
+  signOut(auth)
+    .then(() => {
+      alert("user signed out");
+      authWindow.classList.remove("hidden");
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+      authPopupInner.classList.add("swing");
+      errorPopup.classList.remove("hidden");
+      errorPopupTxt.textContent = errorMessage;
+      setTimeout(() => {
+        authPopupInner.classList.remove("swing");
+      }, 500);
+    });
+});
+
+//сохранение данных в о карточках в бд
+
+import * as Model from "./model.js";
+
+function pushDataToDb() {
+  const model = Model.getModel();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      const userId = user.uid;
+      update(ref(database, "users/" + userId), {
+        model,
+      });
+    } else {
+      console.log("Не войдено");
+    }
+  });
+}
+
+Model.addEventListenerOnModelChanged(pushDataToDb);
