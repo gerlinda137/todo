a5b55dcabd10f93eb95db2bd1b9bd2e2e0d8e2d1
Angelina Baranova <angelinabar52@gmail.com>
2022-06-19 18:17:58 +0300

    clean up

--- view.js
+++ view.js
@@ -170,4 +170,25 @@ function getEditedTaskData() {
   };
 }
 
-//
+// auth
+
+const authWindow = document.querySelector(".auth");
+const signUpBtn = authWindow.querySelector("#signup-btn");
+const logInBtn = authWindow.querySelector("#login-btn");
+const authForm = authWindow.querySelector("#authForm");
+const signUpForm = authWindow.querySelector("#signUpForm");
+const authTitle = authWindow.querySelector(".auth__title");
+const authInputs = authWindow.querySelector(".auth__inputs");
+
+function transformAuthWindow(titleTxt, hidingForm, revealingForm) {
+  authTitle.textContent = titleTxt;
+  hidingForm.classList.add("hidden");
+  revealingForm.classList.remove("hidden");
+}
+
+signUpBtn.addEventListener("click", () =>
+  transformAuthWindow("Sign Up", authForm, signUpForm)
+);
+logInBtn.addEventListener("click", () =>
+  transformAuthWindow("Log In", signUpForm, authForm)
+);
